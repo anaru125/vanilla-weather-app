8c4afa644352c21bb22d994aefdfdd15d90db7dd
anaru125 <91435238+anaru125@users.noreply.github.com>
2021-12-13 12:38:54 -0800

    inject API for current forecast into Java

--- project.js
+++ project.js
@@ -21,6 +21,45 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class ="row">`;
+  let days = ["Sun", "Mon", "Tues", "Weds", "Thurs"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+          <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img src="http://openweathermap.org/img/wn/01d@2x.png"
+             alt="" 
+             width="42"
+             />
+             <div class = "weather-forecast-temperatures">
+            <span class="weather-forecast-temperatures-max">
+             18°
+             </span>
+             <span class= "weather-forecast-temperatures-min">
+             12°
+             </span>
+        </div>
+      </div>
+       `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "94284f34da3f554d78d6efee7cf6dcce";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -43,6 +82,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
